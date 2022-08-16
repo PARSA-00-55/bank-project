dec7842bc33c6f7ebc81bd4f4841651d1fa92fb2
PARSA-00-55 <parsa02345@gmail.com>
2022-08-16 20:17:30 +0600

    js completed

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//add event handlar to the button
+const button = document.getElementById('log-in-btn');
+button.addEventListener('click', function(){
+    //step-2 : get the email address from the email input field
+    const userEmailField = document.getElementById('user-email');
+    const userEmail = userEmailField.value;
+
+    //step-3 : get the password from the password from password input field
+    const userPasswordField = document.getElementById('user-password');
+    const userPassword = userPasswordField.value;
+
+    if(userEmail === 'admin@gmail.com' && userPassword === 'admin1234'){
+        location.href = "bank-page.html";
+    }else{
+        alert('Email & password not matched')
+    }
+})
